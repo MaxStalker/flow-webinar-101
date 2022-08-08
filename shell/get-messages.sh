6993e9615398b4bdc33354bbf965fedefbbea019
Max Daunarovich <max.daunarovich@gmail.com>
2022-08-08 17:47:44 +0300

    Add shell scripts

---
+++ shell/get-messages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+flow scripts execute ./cadence/scripts/get-messages.cdc $1 --network=testnet
---
+++ shell/send-message.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+flow transactions send ./cadence/transactions/sign-yearbook.cdc $1 "$2" --network=testnet --signer=first
